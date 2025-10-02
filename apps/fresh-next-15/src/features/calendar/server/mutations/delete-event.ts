94770f1acd0d39f69116001e12a3ce7a21d00439
Remco Stoeten <remcostoeten@hotmail.com>
2025-10-02 23:39:32 +0200

    move apps

--- apps/fresh-next-15/src/features/calendar/server/mutations/delete-event.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "@/server/db"
-import { events } from "@/server/schema"
-import { eq } from "drizzle-orm"
-
-export async function deleteEvent(eventId: string) {
-  const [deletedEvent] = await db.delete(events).where(eq(events.id, eventId)).returning()
-
-  return deletedEvent
-}
--- apps/fresh-next-15/src/features/calendar/server/queries/get-event.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "@/server/db"
-import { events } from "@/server/schema"
-import { eq } from "drizzle-orm"
-
-export async function getEvent(eventId: number) {
-  const [event] = await db.select().from(events).where(eq(events.id, eventId)).limit(1)
-
-  return event
-}
